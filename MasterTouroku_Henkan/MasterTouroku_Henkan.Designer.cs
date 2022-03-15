e86e7a505c51aba8b7572c8be36db7edeeb8ed00
capital-phyoethurakyaw <capital.phyoethurakyaw@gmail.com>
2022-03-15 18:12:08 +0630

    PTK

---
+++ MasterTouroku_Henkan/MasterTouroku_Henkan.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace MasterTouroku_Henkan
+{
+    partial class MasterTouroku_Henkan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.PanelHeader.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PanelHeader
+            // 
+            this.PanelHeader.Controls.Add(this.panel1);
+            this.PanelHeader.Size = new System.Drawing.Size(1711, 144);
+            this.PanelHeader.Controls.SetChildIndex(this.panel1, 0);
+            // 
+            // PanelSearch
+            // 
+            this.PanelSearch.Location = new System.Drawing.Point(1177, 0);
+            // 
+            // btnChangeIkkatuHacchuuMode
+            // 
+            this.btnChangeIkkatuHacchuuMode.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Location = new System.Drawing.Point(40, -20);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1000, 160);
+            this.panel1.TabIndex = 100;
+            // 
+            // MasterTouroku_Henkan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1713, 887);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.ModeVisible = true;
+            this.Name = "MasterTouroku_Henkan";
+            this.PanelHeaderHeight = 200;
+            this.Text = "MasterTouroku_Henkan";
+            this.Load += new System.EventHandler(this.MasterTouroku_Henkan_Load);
+            this.PanelHeader.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
